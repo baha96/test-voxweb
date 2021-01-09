75202061556ad3b1bda91ece0b1bbf57606064f7
baha96 <baha96.dev@gmail.com>
2021-01-10 01:14:46 +0600

    add page statistics

---
+++ front/pages/user/statistics.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="statistics">
+      statistics
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "statistics"
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
